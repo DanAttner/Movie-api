a8d01e07e19457cbeb065b1f1fe3abece2be6b6a
HarveyBirdman <85701217+DanAttner@users.noreply.github.com>
2021-10-27 14:13:32 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,9 +15,7 @@ let generateJWTToken = (user) => {
 
 /* POST login. */
 module.exports = (router) => {
-  console.log("test 1")
   router.post("/login", (req, res) => {
-    console.log("test 2");
     passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
